140c78c5ac010f6ca233d4a7f49f3a61225851b9
lisa <elizaveta.guseva@stonybrook.edu>
2015-01-04 20:09:15 -0500

    writing ODE. steady state finished. sometimes returns < 0 otherwise sensible 	modified:   IFode.py 	modified:   IFpolymersSteady.py

--- IFode.py
+++ IFode.py
@@ -6,15 +6,18 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 def G(x,sigma):
+    '''Gaussian distribution'''
     return 1.0/(sigma*sqrt(2*pi))*exp(-x/(2*sigma**2))
 
 def Delta(x,y):
+    '''cronecker delta'''
     if x==y:
         return 1
     else:
         return 0
 
 def Nkdd(l,k,df,dt):
+    '''number of sequences which are on the dt shell in distance k away from df shell'''
     nkdd = 0
     for i in range(0,min(df,dt)+1):
         nkdd+=sp.binom(df,min(df,dt)-i)*sp.binom(l-df,max(0,dt-df)+i)*Delta(k,abs(dt-df)+2*i)
@@ -22,10 +25,25 @@ def Nkdd(l,k,df,dt):
     return nkdd
     
 def Pkdd(l,k,df,dt):
+    '''probability to make a jump of length k from df shell into dt shell
+    '''
     return Nkdd(l,k,df,dt)/sp.binom(l,k)
 
 def Pdd(l,df,dt,sigma):
+    '''probability to make a jump from df to dt'''
     pdd=0
     for k in range(0,l+1):
         pdd+=Pkdd(l,k,df,dt)*G(k,sigma)
     return pdd
+
+def dFdt(l,m,sigma,ki,kf,I,F):
+    dFdt=[]
+    for d in range(l+1):
+        dFddt=m-F[d]
+        for i in range(l+1):
+            dFddt+=ki*I[i]*Pdd(l,i,d,sigma)
+        dFdt.append(dFddt)
+    
+    return dFdt
+
+
--- IFpolymersSteady.py
+++ IFpolymersSteady.py
@@ -3,16 +3,20 @@ import numpy as np
 from math import *
 from scipy import special as sp
 
+
 def G(x,sigma):
+    '''Gaussian distribution'''
     return 1.0/(sigma*sqrt(2*pi))*exp(-x/(2*sigma**2))
 
 def Delta(x,y):
+    '''cronecker delta'''
     if x==y:
         return 1
     else:
         return 0
 
 def Nkdd(l,k,df,dt):
+    '''number of sequences which are on the dt shell in distance k away from df shell'''
     nkdd = 0
     for i in range(0,min(df,dt)+1):
         nkdd+=sp.binom(df,min(df,dt)-i)*sp.binom(l-df,max(0,dt-df)+i)*Delta(k,abs(dt-df)+2*i)
@@ -20,35 +24,41 @@ def Nkdd(l,k,df,dt):
     return nkdd
     
 def Pkdd(l,k,df,dt):
+    '''probability to make a jump of length k from df shell into dt shell
+    '''
     return Nkdd(l,k,df,dt)/sp.binom(l,k)
 
 def Pdd(l,df,dt,sigma):
+    '''probability to make a jump from df to dt'''
     pdd=0
     for k in range(0,l+1):
         pdd+=Pkdd(l,k,df,dt)*G(k,sigma)
     return pdd
 
-def CoeffB(l,b,df,sigma,rSigma,K):
+def CoeffB(l,b,df,sigma,rSigma,ki,kf):
+    ''' an element of the matrix of coefficients for an ode, produced from the probabilities above '''
     cfb=0
     for dt in range(0,l+1):
-        cfb+=G(dt,rSigma)*Pdd(l,df,dt,sigma)*Pdd(l,dt,b,sigma)*K
+        cfb+=G(dt,rSigma)*Pdd(l,df,dt,sigma)*Pdd(l,dt,b,sigma)*ki*kf
     
     return cfb
 
-def getA(l,sigma,rSigma,K):
+def getA(l,sigma,rSigma,ki,kf):
+    ''' returns matrix mentioned above '''
     aL=[]
     for df in range(0,l+1):
         aL.append([])
         for b in range(0,l+1):
             if not b==df:
-                aL[df].append(CoeffB(l,b,df,sigma,rSigma,K))
+                aL[df].append(CoeffB(l,b,df,sigma,rSigma,ki,kf))
             else:
-                aL[df].append(CoeffB(l,b,df,sigma,rSigma,K)-1)
+                aL[df].append(CoeffB(l,b,df,sigma,rSigma,ki,kf)-1)
     A=np.array(aL)
     
     return A
 
 def getPddMatrix(l,sigma):
+    ''' returns the matrix of Pdd's mentioned above '''
     pM=[]
     for d1 in range(l+1):
         pM.append([])
@@ -57,17 +67,30 @@ def getPddMatrix(l,sigma):
     
     return pM
 
-l = 2
+def getB(l,m,kf,rSigma):
+    B=[]
+    for d in range(l+1):
+        Bd=-m
+        for i in range(l+1):
+            Bd+=m*kf*G(i,rSigma)*Pdd(l,i,d,sigma)
+        B.append(Bd)
+        
+    
+    return np.array(B) 
 
-sigma = 1.0
-rSigma = 2.0
-K = 1.0
-b = np.array([0.0]*(l+1))
-A = getA(l,sigma,rSigma,K)
-print(A)
-def null(A, eps=1e-15):
-    u, s, vh = np.linalg.svd(A)
-    null_space = np.compress(s <= eps, vh, axis=0)
-    return null_space.T
+l=5
+sigma=1.0
+rSigma=0.5
+ki=2.0
+kf=3.0
+m=2.0
 
-print(null(A).T)
+A=getA(l,sigma,rSigma,ki,kf)
+B=getB(l,m,kf,rSigma)
+
+    
+x = np.linalg.solve(A, B)    
+print(x)
+
+
+
