ca7b0db032c0753d985dc430018583ab49662b50
lisa <elizaveta.guseva@stonybrook.edu>
2015-01-07 17:09:35 -0500

    matplotlib... 	modified:   IFode.py

--- IFode.py
+++ IFode.py
@@ -4,9 +4,9 @@ import sys
 from math import *
 from scipy import special as sp
 import matplotlib.pyplot as plt
-import matplotlib.use as plotuse
+import matplotlib
 # Force matplotlib to not use any Xwindows backend.
-plotuse('Agg')
+matplotlib.use('Agg')
 from scipy.integrate import odeint
 
 def G(x,sigma):
