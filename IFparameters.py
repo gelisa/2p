06a8c4c5d71d114e6cd2715f1cdafec80ec47b5b
lisa <elizaveta.guseva@stonybrook.edu>
2015-01-07 17:30:50 -0500

    small fix

--- IFparameters.py
+++ IFparameters.py
@@ -1,11 +1,11 @@
 #!/home/eliza/anaconda3/bin/python3.4
 import os
-L = [15 30]
-M = [1.0 3.0 10.0]
-SIGMA = [0.3 1.0 3.0 10.0]
-RSIGMA = [0.3 1.0 3.0 10.0]
-KI = [1.0 3.0 10.0]
-KF = [1.0 3.0 10.0]
+L = [15, 30]
+M = [1.0, 3.0, 10.0]
+SIGMA = [0.3, 1.0, 3.0, 10.0]
+RSIGMA = [0.3, 1.0, 3.0, 10.0]
+KI = [1.0, 3.0, 10.0]
+KF = [1.0, 3.0, 10.0]
 
 def writesh(l,m,sigma,rSigma,ki,kf):
     filename= 's'+str(l)+'-'+str(m)+'-'+str(sigma)+'-'+str(rSigma)+'-'+str(ki)+'-'+str(kf)+'.sh'
